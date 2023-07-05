b072a4e8b33caa6b7303a7a6530d7efaa400bc37
Abhi Yerra <abhi@berkeley.edu>
2023-07-05 13:56:38 -0700

    Added components

--- main.tf
+++ main.tf
@@ -22,6 +22,8 @@ resource "google_container_cluster" "cluster" {
   }
 
   monitoring_config {
+    enable_components = [ "SYSTEM_COMPONENTS" ]
+
     managed_prometheus {
       enabled = false
     }
